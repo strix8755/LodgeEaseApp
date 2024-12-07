c64d7908a50653edf8c3defff2a123cc6a348b3e
akiak <rjbabygo02@gmail.com>
2024-12-07 23:08:14 +0800

    Client page connected additions/fixed

--- ClientSide/Dashboard/app.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    // Hide all content sections
-    document.querySelectorAll('.content-section').forEach(section => {
-        section.classList.add('hidden');
-    });
-    
-    // Show selected section
-    document.getElementById(sectionId).classList.remove('hidden');
-}
